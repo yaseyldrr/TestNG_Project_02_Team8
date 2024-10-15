df5defe30b75c0c5d908c8461e4273e409c3281e
Abylmyrat Gummanov <abdy.coding@gmail.com>
2024-10-15 13:53:16 +0300

    User story 2 is done. (Cross Campus) Login button and main screen Part.

--- src/TC_02/TC_02.java
+++ src/TC_02/TC_02.java
@@ -1,4 +1,19 @@
 package TC_02;
 
-public class TC_02 {
+import Utility.BaseDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TC_02 extends BaseDriver {
+    @Test(groups = "Smoke Test")
+            public void Test() {
+
+
+        TC_02Elements tc = new TC_02Elements();
+
+        driver.get("https://techno.study/tr/");
+        tc.camPusLogin.click();
+        Assert.assertTrue(driver.getCurrentUrl().contains("https://campus.techno.study/"), "The url doesn't match the expected one");
+
+    }
 }
--- src/TC_02/TC_02Elements.java
+++ src/TC_02/TC_02Elements.java
@@ -1,4 +1,16 @@
 package TC_02;
 
+import Utility.BaseDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class TC_02Elements {
+    public TC_02Elements() {
+        PageFactory.initElements(BaseDriver.driver, this);
+    }
+
+    @FindBy(xpath = "//a[text()='Campus Login']")
+    public WebElement camPusLogin;
+
 }
