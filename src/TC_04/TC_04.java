7b46d3076381e91b50061f2ea25371d16e786c59
Yeldar <ildar1zet@gmail.com>
2024-10-17 14:57:28 -0400

    Kurs menu validation

--- src/TC_04/TC_04.java
+++ src/TC_04/TC_04.java
@@ -1,4 +1,26 @@
 package TC_04;
 
-public class TC_04 {
+import Utility.BaseDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TC_04 extends BaseDriver {
+    @Test
+    public void kurslarMenusu() throws InterruptedException {
+        TC_04Elements elements = new TC_04Elements();
+        driver.get("https://techno.study/tr/");
+        elements.kurslarButton.click();
+
+        System.out.println(elements.kurslarMenuItems.size());
+        for (int i = 0;i< elements.kurslarMenuItems.size();i++){
+            Assert.assertTrue(elements.kurslarMenuItems.get(i).isDisplayed());
+        }
+
+        for (int i = 0;i< elements.kurslarMenuItems.size();i++){
+        elements.kurslarMenuItems.get(i).click();
+        Assert.assertTrue(driver.getCurrentUrl().contains(elements.kurslarMenuItems.get(i).getText()));
+        driver.navigate().back();
+        }
+
+    }
 }
--- src/TC_04/TC_04Elements.java
+++ src/TC_04/TC_04Elements.java
@@ -1,4 +1,21 @@
 package TC_04;
 
+import Utility.BaseDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
+
 public class TC_04Elements {
+    public TC_04Elements() {
+        PageFactory.initElements(BaseDriver.driver,this);
+    }
+    @FindBy(xpath = "(//a[.='Kurslar'])[1]")
+    public WebElement kurslarButton;
+
+    @FindBy(css = "[role='menuitem']>span")
+    public List<WebElement> kurslarMenuItems;
+
+
 }
