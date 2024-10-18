61cb4003b72022fc6d7483fb2d426f753b7bbe2e
gul <kgulmeiram@gmail.com>
2024-10-18 07:19:00 +0300

    TC05

--- src/TC_05/TC_05.java
+++ src/TC_05/TC_05.java
@@ -1,4 +1,36 @@
 package TC_05;
 
-public class TC_05 {
+import Utility.BaseDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class TC_05 extends BaseDriver {
+    @Test
+    public void Test(){TC_05Elements tc=new TC_05Elements();
+        driver.get("https://techno.study/tr/");
+        driver.manage().window().maximize();
+
+        JavascriptExecutor js=(JavascriptExecutor)driver;
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+        js.executeScript("arguments[0].scrollIntoView(true);", tc.dw);
+        js.executeScript("window.scrollTo(0, document.body.scrollHeight);");
+
+        tc.facebook.click();
+        driver.navigate().back();
+        tc.inst.click();
+        driver.navigate().back();
+        tc.youtube.click();
+        driver.navigate().back();
+        tc.linkedin.click();
+
+
+
+    }
+
 }
--- src/TC_05/TC_05Elements.java
+++ src/TC_05/TC_05Elements.java
@@ -1,4 +1,28 @@
 package TC_05;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import Utility.BaseDriver;
+import org.openqa.selenium.support.PageFactory;
 
 public class TC_05Elements {
+    public TC_05Elements() {
+
+        PageFactory.initElements(BaseDriver.driver, this);
+    }
+    @FindBy (xpath = "//*[@id='rec684017893']")
+    public WebElement dw;
+
+    @FindBy (xpath = "//a[@href='https://www.facebook.com/technostudy.turkiye']")
+    public WebElement facebook;
+
+    @FindBy(xpath = "//a[@href='https://www.instagram.com/technostudy.tr/']")
+    public WebElement inst;
+
+    @FindBy(xpath = "//a[@href='https://www.youtube.com/@TechnoStudyTR']")
+    public WebElement youtube;
+
+    @FindBy(xpath = "//a[@href='https://www.linkedin.com/company/techno-study-tr/']")
+    public WebElement linkedin;
+
+
 }
