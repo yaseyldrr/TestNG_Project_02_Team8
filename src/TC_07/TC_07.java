2199203660f53c65e822c6abdb338793eabe6849
yaseyldrr <116019129+yaseyldrr@users.noreply.github.com>
2024-10-20 12:24:36 +0300

    TC_07 code is created

--- src/TC_07/TC_07.java
+++ src/TC_07/TC_07.java
@@ -1,4 +1,49 @@
 package TC_07;
 
-public class TC_07 {
+import TC_06.TC_06Elements;
+import Utility.BaseDriver;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TC_07 extends BaseDriver {
+    @Test(groups = {"Smoke Test"})
+    public void Test() {
+        TC_07Elements tc = new TC_07Elements();
+
+        driver.get("https://techno.study/tr/");
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
+
+        List<WebElement> courses = new ArrayList<>();
+        courses.add(tc.sdet);
+        courses.add(tc.androidapp);
+        courses.add(tc.veribilimi);
+
+        for (WebElement e : courses) {
+            Assert.assertTrue(e.isDisplayed());
+
+        }
+        for (WebElement e : courses) {
+            assertIcon(e);
+            driver.navigate().back();
+        }
+    }
+
+    public void assertIcon(WebElement element) {
+        TC_07Elements tc = new TC_07Elements();
+        String url = driver.getCurrentUrl();
+        String[] words = url.split("/");
+        String lastWord = words[words.length - 1];
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("arguments[0].click();", element);
+        Assert.assertTrue(wait.until(ExpectedConditions.urlContains(lastWord)));
+        Assert.assertTrue(wait.until(ExpectedConditions.visibilityOf(tc.icon)).isDisplayed());
+    }
+
 }
--- src/TC_07/TC_07Elements.java
+++ src/TC_07/TC_07Elements.java
@@ -1,4 +1,24 @@
 package TC_07;
 
+import Utility.BaseDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class TC_07Elements {
+    public TC_07Elements() {
+        PageFactory.initElements(BaseDriver.driver, this);
+    }
+
+    @FindBy(xpath = "//*[@id='rec515963420']/div/div/div[24]/a")
+    public WebElement sdet;
+
+    @FindBy(xpath = "//*[@id='rec515963420']/div/div/div[25]/a")
+    public WebElement androidapp;
+
+    @FindBy(xpath = "//*[@id='rec515963420']/div/div/div[23]/a")
+    public WebElement veribilimi;
+
+    @FindBy(xpath = "//img[@imgfield='img']")
+    public WebElement icon;
 }
